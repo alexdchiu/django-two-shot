cfef8e429f480d1157fed32366e2eef72dba71f4
Alex Chiu <alexdchiu@gmail.com>
2022-05-04 19:18:06 -0400

    created accounts app

--- expenses/urls.py
+++ expenses/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, reverse_lazy
 from django.views.generic.base import RedirectView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,5 +25,7 @@ urlpatterns = [
         "",
         RedirectView.as_view(url=reverse_lazy("receipts_list")),
         name="home",
-    )
+    ),
+    path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
+    path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
