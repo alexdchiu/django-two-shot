1824d3b231033259fcfc5ad9cd5f8f36f12c2e65
Alex Chiu <alexdchiu@gmail.com>
2022-05-05 13:50:22 -0400

    added autologin after signup

--- accounts/views.py
+++ accounts/views.py
@@ -2,12 +2,17 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
-
+from django.contrib.auth import login
 
 # Create your views here.
 
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy("login")
+    success_url = reverse_lazy("receipts_list")
     template_name = "registration/signup.html"
+
+    def form_valid(self, form):
+        to_return = super().form_valid(form)
+        login(self.request, self.object)
+        return to_return
